6cce2973238cdab283a15bb648bef949e076639e
Manav Misra <manavm@visionwebsoft.com>
2018-09-28 06:47:36 -0500

    Add girections for using Node.

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Write scripts here when working directly in Node (not in browser).
+ * In other words, write your code and then open up a terminal and do 'node index.js' to run your code.
+ */
