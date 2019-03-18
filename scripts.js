8e6f185c0b3428a4e987fd5c3ca55892a80183ff
Manav Misra <manavm@visionwebsoft.com>
2019-03-18 13:34:34 -0500

    Update line endings and comments.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /**
  * 1. LISTEN for and HANDLE the 'submit' event.
  * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s).
- * 3. Be sure that both fields get concatenated into 1 'String.'
- * 4. Add those to an 'Array' that will hold all of the student names.
- * 5. Iterate over said 'Array', passing each of the names as arguments into console.log().
+ * 3. Add fname and lname into its own Object.
+ * 4. Push the new Object onto an Array
+ * 5. Randomly select a 'student Object' from the Array and console.log().
  */
