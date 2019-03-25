e528b6e10ebc7a06b4735b8f76a8d0c0a6445204
ryan betts <ryan.betts@student.swic.edu>
2019-03-25 13:14:57 -0500

    refactor fxn to fat arrow

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 /**
  * 1. LISTEN for and HANDLE the 'submit' event.
- * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s).
- * 3. Add fname and lname into its own Object.
- * 4. Push the new Object onto an Array
- * 5. Randomly select a 'student Object' from the Array and console.log().
+
  */
 const students = [
   { fname: " Ryan", lname: "Bettss" },
@@ -12,7 +9,8 @@ const students = [
   { fname: " Will", lname: "Bts" },
   { fname: " Phil", lname: "Btts" }
 ];
-function submitCreator(fname, lname) {
+
+const studentCreator = (fname, lname) => {
   // object
   const student = {
     fname,
@@ -20,7 +18,7 @@ function submitCreator(fname, lname) {
   };
 
   return student;
-}
+};
 // function getRandomIntInclusive(min, max) {
 // return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
@@ -34,7 +32,7 @@ document.querySelector("form").addEventListener("submit", e => {
   const fname = document.querySelector("#fname").Value;
   const lname = document.querySelector("#lname").Value;
 
-  students.push(submitCreator(fname, lname));
+  students.push(studentCreator(fname, lname));
 });
 
 document.querySelector("button").addEventListener("click", () => {
